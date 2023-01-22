561e26411f0f084ef02c1e8939880bc7fb7596ed
Deepanshu Somani <dsomani125@gmail.com>
2023-01-22 17:56:23 +0530

    removed useEffect

--- modules/home/components/CreatePost.tsx
+++ modules/home/components/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Button, Input, Dropdown } from '@nextui-org/react';
 import { useState } from 'react';
